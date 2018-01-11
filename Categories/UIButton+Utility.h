5f1e745a6ee119508d3bd984b7b5d47eb6cd7db5
yuchenggui <yuchenggui@58ganji.com>
2018-01-11 11:23:15 +0800

    整理目录

---
+++ Categories/UIButton+Utility.h
@@ -0,0 +1,33 @@
+//
+//  UIButton+Utility.h
+//  CommonlyUtilityDemo
+//
+//  Created by Stone.Yu on 2018/1/10.
+//  Copyright © 2018年 Stone.Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, LYButtonLayoutStyle) {
+    LYButtonLayoutStyleVerticalImageFromTop,
+    LYButtonLayoutStyleVerticalImageFromBottom,
+    LYButtonLayoutStyleHorizontalImageFromLeft,
+    LYButtonLayoutStyleHorizontalImageFromRight
+};
+
+@interface UIButton (Utility)
+
++ (UIButton *)buttonWithTitleFont:(UIFont *)font titleColor:(UIColor *)color;
++ (UIButton *)buttonWithTitleFont:(UIFont *)font titleColor:(UIColor *)color title:(NSString *)title;
++ (UIButton *)buttonWithTitleFont:(UIFont *)font titleColor:(UIColor *)color backgroundColor:(UIColor *)backColor;
++ (UIButton *)buttonWithTitleFont:(UIFont *)font titleColor:(UIColor *)color title:(NSString *)title backgroundColor:(UIColor *)backColor;
+
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color;
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color title:(NSString *)title;
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color backgroundColor:(UIColor *)backColor;
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color title:(NSString *)title backgroundColor:(UIColor *)backColor;
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color normalTitle:(NSString *)normalTitle selectedTitle:(NSString *)selectedTitle;
+
+- (void)layoutWithStyle:(LYButtonLayoutStyle)style andSpacing:(CGFloat)spacing;
+
+@end
---
+++ Categories/UIButton+Utility.m
@@ -0,0 +1,118 @@
+//
+//  UIButton+Utility.m
+//  CommonlyUtilityDemo
+//
+//  Created by Stone.Yu on 2018/1/10.
+//  Copyright © 2018年 Stone.Yu. All rights reserved.
+//
+
+#import "UIButton+Utility.h"
+
+@implementation UIButton (Utility)
+
++ (UIButton *)buttonWithTitleFont:(UIFont *)font titleColor:(UIColor *)color
+{
+    return [UIButton buttonWithTitleFont:font titleColor:color title:nil backgroundColor:nil];
+}
+
++ (UIButton *)buttonWithTitleFont:(UIFont *)font titleColor:(UIColor *)color title:(NSString *)title
+{
+    return [UIButton buttonWithTitleFont:font titleColor:color title:title backgroundColor:nil];
+}
+
++ (UIButton *)buttonWithTitleFont:(UIFont *)font titleColor:(UIColor *)color backgroundColor:(UIColor *)backColor
+{
+    return [UIButton buttonWithTitleFont:font titleColor:color title:nil backgroundColor:backColor];
+}
+
++ (UIButton *)buttonWithTitleFont:(UIFont *)font titleColor:(UIColor *)color title:(NSString *)title backgroundColor:(UIColor *)backColor
+{
+    UIButton *button = [[UIButton alloc] init];
+    
+    [button setTitleFont:font titleColor:color title:title backgroundColor:backColor];
+    
+    return button;
+}
+
+
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color
+{
+    [self setTitleFont:font titleColor:color title:nil backgroundColor:nil];
+}
+
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color title:(NSString *)title
+{
+    [self setTitleFont:font titleColor:color title:title backgroundColor:nil];
+}
+
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color backgroundColor:(UIColor *)backColor
+{
+    [self setTitleFont:font titleColor:color title:nil backgroundColor:backColor];
+}
+
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color title:(NSString *)title backgroundColor:(UIColor *)backColor;
+{
+    if (font) {
+        [self.titleLabel setFont:font];
+    }
+    
+    if (color) {
+        [self setTitleColor:color forState:UIControlStateNormal];
+    }
+    
+    if (title) {
+        [self setTitle:title forState:UIControlStateNormal];
+    }
+    
+    if (backColor) {
+        [self setBackgroundColor:backColor];
+    }
+}
+
+
+- (void)setTitleFont:(UIFont *)font titleColor:(UIColor *)color normalTitle:(NSString *)normalTitle selectedTitle:(NSString *)selectedTitle
+{
+    [self setTitleFont:font titleColor:color title:normalTitle backgroundColor:nil];
+    
+    if (selectedTitle) {
+        [self setTitle:selectedTitle forState:UIControlStateSelected];
+    }
+}
+
+
+
+- (void)layoutWithStyle:(LYButtonLayoutStyle)style andSpacing:(CGFloat)spacing
+{
+    CGSize imageSize = self.imageView.frame.size;
+    CGSize titleSize = self.titleLabel.frame.size;
+    
+    switch (style) {
+        case LYButtonLayoutStyleVerticalImageFromTop: {
+            self.imageEdgeInsets = UIEdgeInsetsMake(-(titleSize.height + spacing) / 2, titleSize.width / 2, (titleSize.height + spacing) / 2, -(titleSize.width / 2));
+            self.titleEdgeInsets = UIEdgeInsetsMake((imageSize.height + spacing) / 2, -(imageSize.width / 2), -(imageSize.height + spacing) / 2, imageSize.width / 2);
+        }
+            break;
+            
+        case LYButtonLayoutStyleVerticalImageFromBottom: {
+            self.imageEdgeInsets = UIEdgeInsetsMake((titleSize.height + spacing) / 2, titleSize.width / 2, -(titleSize.height + spacing) / 2, -(titleSize.width / 2));
+            self.titleEdgeInsets = UIEdgeInsetsMake(-(imageSize.height + spacing) / 2, -(imageSize.width / 2), (imageSize.height + spacing) / 2, imageSize.width / 2);
+        }
+            break;
+            
+        case LYButtonLayoutStyleHorizontalImageFromLeft: {
+            self.imageEdgeInsets = UIEdgeInsetsMake(0.0, -(spacing / 2), 0.0, spacing / 2);
+            self.titleEdgeInsets = UIEdgeInsetsMake(0.0, spacing / 2, 0.0, -(spacing / 2));
+        }
+            break;
+            
+        case LYButtonLayoutStyleHorizontalImageFromRight: {
+            self.imageEdgeInsets = UIEdgeInsetsMake(0.0, titleSize.width + spacing / 2, 0.0, -(titleSize.width + spacing / 2));
+            self.titleEdgeInsets = UIEdgeInsetsMake(0.0, -(imageSize.width + spacing / 2), 0.0, (imageSize.width + spacing / 2));
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+@end
